f964efcde74f9921b44b6d5410ca715f074f2693
kamesh532 <532kamesh@gmail.com>
2025-04-15 12:49:29 +0530

    changess in nonrepeat program

--- RestAssuredAutomation/src/test/java/JavaPrograms/NonRepeatingElements.java
+++ RestAssuredAutomation/src/test/java/JavaPrograms/NonRepeatingElements.java
@@ -3,20 +3,17 @@ package JavaPrograms;
 import org.testng.reporters.jq.Main;
 
 public class NonRepeatingElements {
-	public static void nonRepeated(int a[]) {
-		
-		int size=a.length, res=0;
-		for(int i=0; i<size; i++)
-		{
-			 res=res^a[i];
-	}
-		System.out.println(res);
-		
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[] = { 2, 3,3,7,2,5, 5, 7, 4,4 ,9,1};
-		nonRepeated(arr);
-	}
+	 public static void main(String[] args) {
+	      String str="swiss";
+	      for(char ch:str.toCharArray())
+	      {
+	          if(str.indexOf(ch)==str.lastIndexOf(ch))
+	            {
+	                System.out.println("First Non-Repeating: " + ch);
+	                break;
+	            }
+	      }
+	         
+	    }
 
 }
